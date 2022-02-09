6eec89c20c021c104180d4d668a51fd27c9e1229
Vy Nguyen <93696226+thucvy@users.noreply.github.com>
2022-02-09 18:06:02 -0500

    Create main.py

---
+++ ISSOverheadNotifier_API/main.py
@@ -0,0 +1,50 @@
+import requests
+import smtplib
+from datetime import datetime
+import time
+
+# FILL YOUR OWN EMAIL AND PASSWORD (GMAIL)
+my_email = "****"
+password = "****"
+
+
+MY_LAT = 51.507351 # Your latitude
+MY_LONG = -0.127758 # Your longitude
+
+response = requests.get(url="http://api.open-notify.org/iss-now.json")
+response.raise_for_status()
+data = response.json()
+
+iss_latitude = float(data["iss_position"]["latitude"])
+iss_longitude = float(data["iss_position"]["longitude"])
+
+#Your position is within +5 or -5 degrees of the ISS position.
+iss_overhead = MY_LAT-5 <= iss_latitude <= MY_LAT+5 and MY_LONG-5 <= iss_longitude <= MY_LONG+5
+
+parameters = {
+    "lat": MY_LAT,
+    "lng": MY_LONG,
+    "formatted": 0,
+}
+
+response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+response.raise_for_status()
+data = response.json()
+sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+
+time_now = datetime.now()
+hour = time_now.hour
+
+while True:
+    time.sleep(60)# BONUS: run the code every 60 seconds.
+    if iss_overhead:
+        if hour < 7 or hour > 17:
+            with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+                connection.starttls()
+                connection.login(user=my_email, password=password)
+                connection.sendmail(
+                    from_addr=my_email,
+                    to_addrs="lysoju21@yahoo.com",
+                    msg="Subject: ISS Overhead\n\nHey! The ISS is above you in the sky. Look up to see it yourself!"
+                )
